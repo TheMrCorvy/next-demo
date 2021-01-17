1440334969c9e2de8b36b7006568489fcf9badf8
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2021-01-17 18:55:42 -0300

    added some random shapes

--- components/blog/Post.jsx
+++ components/blog/Post.jsx
@@ -1,17 +1,44 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Link from "next/link"
 
-const Post = ({ post }) => (
-	<article className="s-radius-1 s-pxy-4 s-shadow-bottom course-card background s-shadow-card-micro white-color nowrap s-column s-mb-0">
-		<h2>{post.title}</h2>
-		<p>{post.body}</p>
-		<span>{`Escrito por el usuario ${post.userId}`}</span>
+const Post = ({ post }) => {
+	const randomize = () => {
+		let radius = ""
 
-		{/* de esta manera se pueden implementar variables en las url */}
-		<Link href="/blog/[id]" as={`/blog/${post.id}`}>
-			<a>Leer más</a>
-		</Link>
-	</article>
-)
+		const times = [1, 2, 3, 4, 15, 6, 7, 8]
+
+		times.map((time) => {
+			if (time === 4) {
+				radius = radius + Math.floor(Math.random() * (100 - 15 + 15) + 15) + "% /"
+			} else {
+				radius = radius + " " + Math.floor(Math.random() * (100 - 15 + 15) + 15) + "%"
+			}
+		})
+
+		console.log(radius)
+
+		return radius
+	}
+
+	useEffect(() => {
+		randomize()
+	}, [])
+
+	return (
+		<article
+			className="s-pxy-4 s-shadow-bottom course-card background s-shadow-card-micro white-color nowrap s-column s-mb-0"
+			style={{ borderRadius: randomize(), padding: "3rem" }}
+		>
+			<h2>{post.title}</h2>
+			<p>{post.body}</p>
+			<span>{`Escrito por el usuario ${post.userId}`}</span>
+
+			{/* de esta manera se pueden implementar variables en las url */}
+			<Link href="/blog/[id]" as={`/blog/${post.id}`}>
+				<a>Leer más</a>
+			</Link>
+		</article>
+	)
+}
 
 export default Post
